11262e61d4f2ab77071c39e33b250288bbea1fa1
Titus Wormer <tituswormer@gmail.com>
2021-04-26 12:56:32 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+/**
+ * @typedef {import('hast').Element & {tagName: 'a'|'audio'|'canvas'|'datalist'|'dd'|'del'|'ins'|'map'|'menu'|'noscript'|'object'|'ol'|'picture'|'select'|'table'|'tbody'|'tfoot'|'thead'|'tr'|'ul'|'video'}} Transparent
+ * @typedef {import('hast-util-is-element').AssertPredicate<Transparent>} AssertTransparent
+ */
+
 import {convertElement} from 'hast-util-is-element'
 
+/**
+ * Check if a node is a script-supporting element.
+ * @type {AssertTransparent}
+ */
+// @ts-ignore Sure, the assertion matches.
 export const transparent = convertElement([
   'a',
   'audio',
