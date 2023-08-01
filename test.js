5ef622268156720afb853f31c65980356ca5a701
Titus Wormer <tituswormer@gmail.com>
2023-08-01 12:53:51 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,50 +1,60 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {transparent} from './index.js'
-import * as mod from './index.js'
 
-test('transparent', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['transparent'],
-    'should expose the public api'
+test('transparent', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'transparent'
+    ])
+  })
+
+  await t.test('should return `false` without node', async function () {
+    // @ts-expect-error: check how a missing `node` is handled.
+    assert.equal(transparent(), false)
+  })
+
+  await t.test('should return `false` with `null`', async function () {
+    assert.equal(transparent(null), false)
+  })
+
+  await t.test(
+    'should return `false` when without `element`',
+    async function () {
+      assert.equal(transparent({type: 'text'}), false)
+    }
   )
 
-  // @ts-expect-error: check how a missing `node` is handled.
-  assert.equal(transparent(), false, 'should return `false` without node')
-
-  assert.equal(transparent(null), false, 'should return `false` with `null`')
-
-  assert.equal(
-    transparent({type: 'text'}),
-    false,
-    'should return `false` when without `element`'
+  await t.test(
+    'should return `false` when with invalid `element`',
+    async function () {
+      assert.equal(transparent({type: 'element'}), false)
+    }
   )
 
-  assert.equal(
-    transparent({type: 'element'}),
-    false,
-    'should return `false` when with invalid `element`'
+  await t.test(
+    'should return `false` when without not transparent',
+    async function () {
+      assert.equal(
+        transparent({
+          type: 'element',
+          tagName: 'div',
+          children: [{type: 'text', value: 'Alpha'}]
+        }),
+        false
+      )
+    }
   )
 
-  assert.equal(
-    transparent({
-      type: 'element',
-      tagName: 'div',
-      children: [{type: 'text', value: 'Alpha'}]
-    }),
-    false,
-    'should return `false` when without not transparent'
-  )
-
-  assert.equal(
-    transparent({
-      type: 'element',
-      tagName: 'a',
-      properties: {href: '#bravo', title: 'Charlie'},
-      children: [{type: 'text', value: 'Delta'}]
-    }),
-    true,
-    'should return `true` when with transparent'
-  )
+  await t.test('should return `true` when with transparent', async function () {
+    assert.equal(
+      transparent({
+        type: 'element',
+        tagName: 'a',
+        properties: {href: '#bravo', title: 'Charlie'},
+        children: [{type: 'text', value: 'Delta'}]
+      }),
+      true
+    )
+  })
 })
